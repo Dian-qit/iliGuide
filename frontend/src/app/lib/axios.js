276cf71dd1ef056703ae78eeee9d0848981c983c
Vincent Adrian <vincentquitoriano38@gmail.com>
2026-06-26 19:28:33 +0800

    Fix: added env import for deployment

--- frontend/src/app/lib/axios.js
+++ frontend/src/app/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8080/api" : "/api";
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
